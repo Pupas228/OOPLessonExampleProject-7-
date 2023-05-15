d3da39d2d3e69f5819bc82a8a736b166738ee762
pupas228 <ivan.d.dobrovolskiy@gmail.com>
2023-05-15 17:43:33 +0300

    Add project files.

---
+++ OOPLessonExampleProject/teacher.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "main.h"
+
+class Doctor
+{
+privat:
+	string name;
+	string surname;
+	int age;
+	int power;
+public:
+	Doctor();
+	Doctor(string n, string sname, int a, float mark);
+	Doctor(string n, string sname);
+
+	~Doctor();
+
+	string get_name();
+	void set_name(string n);
+	string get_surname();
+	void set_surname(string n);
+	int get_age();
+	void set_age(int a);
+	float get_avg_mark();
+	void set_avg_mark(float mark);
+
+	/*void init(string n, string sname, int a, int power);
+	void init_default();*/
+	string convert_to_string();
+};
